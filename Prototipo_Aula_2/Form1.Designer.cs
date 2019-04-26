974582fb27bd36beb924d26bf5db635a01cf147f
MadWallker <natanaelfpnunes@gmail.com>
2019-04-26 17:21:08 -0300

    Initial commit

---
+++ Prototipo_Aula_2/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Prototipo_Aula_2
+{
+    partial class frmMove
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.Image = global::Prototipo_Aula_2.Properties.Resources.RedGhost;
+            this.Player.Location = new System.Drawing.Point(339, 160);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(53, 50);
+            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pictureBox2.Location = new System.Drawing.Point(598, 95);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(74, 205);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "Obstacle";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pictureBox3.Location = new System.Drawing.Point(114, 95);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(74, 205);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "Obstacle";
+            // 
+            // frmMove
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.Player);
+            this.Name = "frmMove";
+            this.Text = "Movimento de Personagem";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmMove_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmMove_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Prototipo_Aula_2/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototipo_Aula_2
+{
+    public partial class frmMove : Form
+    {
+        private bool paraEsquerda;
+        private bool paraDireita;
+        private bool paraCima;
+        private bool paraBaixo;
+
+        private bool[] dirs = new bool[4];//left,right,up,down
+        private string dir = "";
+        private int velocidade = 3;
+
+        public frmMove()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VerifyCollision()
+        {
+            foreach (Control obj in Controls)
+            {
+                if(obj is PictureBox && (string)obj.Tag == "Obstacle")
+                {
+                    if (Player.Bounds.IntersectsWith(obj.Bounds))
+                    {
+                        if (paraEsquerda && obj.Location.X <= Player.Location.X)
+                            LockDirection(0);
+
+                        if (paraDireita && obj.Location.X >= Player.Location.X)
+                            LockDirection(1);
+
+                        if (paraCima && obj.Bottom >= Player.Top)
+                            LockDirection(2);
+
+                        if (paraBaixo && obj.Top <= Player.Bottom)
+                            LockDirection(3);
+                    }
+                }
+            }
+        }
+
+        private void LockDirection(int index)
+        {
+            dirs[index] = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (paraEsquerda && !dirs[0])
+            {
+                Player.Left -= velocidade;
+            }
+
+            if (paraDireita && !dirs[1])
+            {
+                Player.Left += velocidade;
+            }
+
+            if (paraCima && !dirs[2])
+            {
+                Player.Top -= velocidade;
+            }
+
+            if (paraBaixo && !dirs[3])
+            {
+                Player.Top += velocidade;
+            }
+        }
+
+        private void frmMove_KeyDown(object sender, KeyEventArgs e)
+        {
+            VerifyCollision();
+
+            //Se o usuário pressionar a tecla para a esquerda
+            if (e.KeyCode == Keys.Left)
+            {
+                paraEsquerda = true;
+            }
+
+            //Se o usuário pressionar a tecla para a direita
+            if (e.KeyCode == Keys.Right)
+            {
+                paraDireita = true;
+            }
+
+            //Se o usuário pressionar a tecla para cima
+            if (e.KeyCode == Keys.Up)
+            {
+                paraCima = true;
+            }
+
+            //Se o usuário pressionar a tecla para baixo
+            if (e.KeyCode == Keys.Down)
+            {
+                paraBaixo = true;
+            }
+        }
+
+        private void frmMove_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                //Utilizando Switch para alterar o valor das variáveis
+                case Keys.Left:
+                    paraEsquerda = false;
+                    break;
+                case Keys.Right:
+                    paraDireita = false;
+                    break;
+                case Keys.Up:
+                    paraCima = false;
+                    break;
+                case Keys.Down:
+                    paraBaixo = false;
+                    break;
+            }
+        }
+    }
+}
